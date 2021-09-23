8b6931689f72cc68e0e9c9e641464ee5adea7142
malrajmund <malrajmund@gmail.com>
2021-09-23 13:39:56 +0200

    Added register modal

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import AppleIcon from "@mui/icons-material/Apple";
@@ -6,8 +6,60 @@ import GoogleIcon from "@mui/icons-material/Google";
 import "./Landing.scss";
 
 export const Landing = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <>
+      <div className={isModalOpen ? "modal--open" : "modal--close"}>
+        <Grid container className='modal__container'>
+          <Grid item xl={1}>
+            <div
+              className='modal__closeButton'
+              onClick={(e) => setIsModalOpen(!isModalOpen)}
+            >
+              <hr className='modal__closeButtonFirstLine'></hr>
+              <hr className='modal__closeButtonSecondLine'></hr>
+            </div>
+          </Grid>
+          <Grid item xl={12} className='modal__twitterIcon'>
+            <TwitterIcon style={{ fontSize: "35px" }} />
+          </Grid>
+          <Grid item xl={12}>
+            <h2>Utwórz konto</h2>
+          </Grid>
+          <Grid item xl={12}>
+            <input
+              className='modal__Input'
+              type='text'
+              placeholder='Imię'
+            ></input>
+          </Grid>
+          <Grid item xl={12}>
+            <input
+              className='modal__Input'
+              type='text'
+              placeholder='Telefon'
+            ></input>
+          </Grid>
+          <Grid item xl={12}>
+            <a href='/'>Użyj adresu e-mail</a>
+          </Grid>
+          <Grid item xl={12}>
+            <p>Data urodzenia</p>{" "}
+            <p>
+              Ta informacja nie będzie widoczna dla innych użytkownikow. Podaj
+              swój wiek, nawet jęsli jest to konto reprezentująće firmę,
+              zwierzaka lub jakąkolwioek inną osobę czy rzecz.
+            </p>
+          </Grid>
+          <Grid item xl={12}>
+            <input></input>
+          </Grid>
+          <Grid item xl={12} className='modal__button'>
+            <button>Zarejestruj się</button>
+          </Grid>
+        </Grid>
+      </div>
       <Grid container className='mainSection'>
         <Grid item xl={7} className='imgContainer'>
           <Grid
@@ -17,7 +69,7 @@ export const Landing = () => {
             className='imgContainer'
           >
             <Grid item xl={4}>
-              <TwitterIcon className='twitterIcon' />
+              <TwitterIcon className='imgContainer__twitterIcon' />
             </Grid>
           </Grid>
         </Grid>
@@ -51,7 +103,10 @@ export const Landing = () => {
               </button>
             </Grid>
             <Grid item xl={12}>
-              <button> Zarejestruj się przez E-Mail</button>
+              <button onClick={(e) => setIsModalOpen(!isModalOpen)}>
+                {" "}
+                Zarejestruj się przez E-mail
+              </button>
             </Grid>
             <Grid item xl={6}>
               <p>
@@ -83,7 +138,7 @@ export const Landing = () => {
           <a href='/'>Blog</a>
           <a href='/'>Status</a>
           <a href='/'>Praca</a>
-          <a href='/'>Zasooby marki</a>
+          <a href='/'>Zasoby marki</a>
           <a href='/'>Reklamy</a>
           <a href='/'>Marketing</a>
           <a href='/'>Twitter dla firm</a>
